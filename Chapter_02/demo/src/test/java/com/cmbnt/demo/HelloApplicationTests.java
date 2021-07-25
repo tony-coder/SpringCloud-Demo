90e82940984a61c6caaca6a165cffb098185fb67
tony-coder <1606771552@qq.com>
2021-07-25 16:48:37 +0800

    增加单元测试

---
+++ Chapter_02/demo/src/test/java/com/cmbnt/demo/HelloApplicationTests.java
@@ -0,0 +1,35 @@
+package com.cmbnt.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+// 使用MockMvc做单元测试的时候，出现了status()、content()、equalTo()这三个方法不能自动引入的情况，不知道是不是IDEA对静态方法的引入有问题，所以，采用了手动引入的方式：
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(classes = DemoApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class HelloApplicationTests {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void hello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+    }
+}
